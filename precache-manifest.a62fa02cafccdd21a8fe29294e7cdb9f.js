ddac20c6a7505f6a90becaa391291e8188dfaf09
SuperC <clarkbros3@gmail.com>
2019-11-16 17:42:09 +0100

    Updates

---
+++ precache-manifest.a62fa02cafccdd21a8fe29294e7cdb9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6afc478f77769f9f23be8094a9cc7dd5",
+    "url": "/SuperC03/reading-list/index.html"
+  },
+  {
+    "revision": "51fa9db96601371d177d",
+    "url": "/SuperC03/reading-list/static/css/main.c58aa3a2.chunk.css"
+  },
+  {
+    "revision": "4637d8ce1cab95b62ccd",
+    "url": "/SuperC03/reading-list/static/js/2.1a3b9191.chunk.js"
+  },
+  {
+    "revision": "51fa9db96601371d177d",
+    "url": "/SuperC03/reading-list/static/js/main.c0814a8a.chunk.js"
+  },
+  {
+    "revision": "5f835a3bfb1b4af6dc2d",
+    "url": "/SuperC03/reading-list/static/js/runtime-main.ad333da4.js"
+  }
+]);
